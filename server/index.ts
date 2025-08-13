d8999f3d7fd1d1236c5675382e27373f3d3d7505
AgtechDS <agtechdesigne@gmail.com>
2025-08-13 11:31:21 +0200

    first commit

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
-import { serveStatic, log } from "./vite";
+import { log } from "./vite";
 
 const app = express();
 app.use(express.json());
@@ -48,7 +48,7 @@ app.use((req, res, next) => {
     throw err;
   });
 
-  serveStatic(app);
+  
 })();
 
 export default app;
